77d672e3066d28bc21263f14e3b37f6b7fbfcd2e
prao582 <81147812+prao582@users.noreply.github.com>
2021-08-15 12:33:17 +1200

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+
+# imports
+from typing import TypeVar
+import numpy as np
+from matplotlib import pyplot as plt
+import math as math
+from scipy.interpolate import interp1d
+
+
+
+
+
+
+if __name__ == "__main__":
+
