076be76cdd8deb6d680f7ba5a00b2d56c58d8dd6
bhal447 <nzben2000@gmail.com>
2021-08-21 11:28:37 +1200

    Update main.py

--- main.py
+++ main.py
@@ -123,71 +123,47 @@ def euler_solve_concentration(f, t0, t1, dt, C0, pars):
 
     return t, C
 
+def improved_euler_concentration(f, t0, t1, dt, C0, pars):
+    ''' Solve an ODE numerically.
 
-# def improved_euler_pressure(f,t0, t1, dt, x0, pars):
-#     """Solve an ODE numerically.
+        Parameters:
+        -----------
+        f : callable
+            Function that returns dxdt given variable and parameter inputs.
+        t0 : float
+            Initial time of concentration.
+        t1 : float
+            Final time of concentration.
+        dt : float
+            Time step length.
+        C0 : float
+            Initial value of concentration.
+        pars : array-like
+            List of parameters passed to ODE function f.
 
-#         Parameters:
-#         -----------
-#         f : callable
-#             Function that returns dxdt given variable and parameter inputs.
-#         t0 : float
-#             Initial time value
-#         t1 : float
-#             Final time value
-#         dt : float
-#             Time step used
-#         x0 : float
-#             Initial value of solution.
-#         pars : array-like
-#             List of parameters passed to ODE function f.
+        Returns:
+        --------
+        t : array-like
+            Independent variable concentration vector.
+        c : array-like
+            Dependent variable concentration vector.
+    '''
 
-#         Returns:
-#         --------
-#         x : array-like
-#             Dependent variable solution vector.
+	# initialise
+    steps = int(np.ceil((t1-t0) / dt))	       	# Number of Euler steps to take
+    t = t0 + np.arange(steps+1) * dt			# t array
+    c = 0. * t						        	# c array to store concentration
+    c[0] = C0							        # Set initial value
+	
+	# Iterate over all values of t
+    for i in range (steps):    
+        f0 = f(t[i], c[i], *pars)
+        f1 = f(t[i] + dt, c[i] + dt * f0, *pars)
+	    # Increment solution by step size x half of each derivative
+        c[i+1] = c[i] + (dt * (0.5 * f0 + 0.5 * f1)) 
 
-#         Notes:
-#         ------
-#         Assume that ODE function f takes the following inputs, in order:
-#             1. independent variable
-#             2. dependent variable
-#             3. Parameters that will later be passed to the curve fitting function
-#             4. all other parameters
-#             Refer to the function definitions for order within (3) and (4)
-#             5. Optional - counter number to be passed to array parameters
-#     """
+    return t, c
 
-#     # Allocate return arrays
-#     t = np.arange(t0, t1+dt, dt)
-#     params_unknown, params_known = pars
-#     x = np.zeros(len(t))
-#     x[0] = x0
-#     i=0
-#     # Check if q is iterable or const
-#     if isinstance(params_known[0], float) == True:
-#         dqdt = 0
-#         if len(params_known) != 2:
-#             params_known.append(dqdt)
-#         # Loop through time values, finding corresponding x value
-#         for i in range(0, (len(t) - 1)):
-#             # Compute normal euler step
-#             x_temp = x[i] + dt*f(t[i], x[i], params_unknown, params_known)
-#             # Corrector step
-#             x[i+1] = x[i] + (dt/2)*(f(t[i], x[i], params_unknown, params_known) + f(t[i+1], x_temp, params_unknown, params_known))
-#     else:
-#         # Get dqdt and append to known parameters
-#         dqdt = np.gradient(params_known[0])
-#         if len(params_known) != 2:
-#             params_known.append(dqdt)
-#         # Loop through time values, finding corresponding x value
-#         for i in range(0, (len(t) - 1)):
-#             # Compute normal euler step
-#             x_temp = x[i] + dt*f(t[i], x[i], params_unknown, params_known, i=i)
-#             # Corrector step
-#             x[i+1] = x[i] + (dt/2)*(f(t[i], x[i], params_unknown, params_known, i=i) + f(t[i+1], x_temp, params_unknown, params_known, i=i))
-        
-#     return x
 
 def plot_given_data():
     year_stock, stock = stock_population()
@@ -305,6 +281,15 @@ def plot_benchmark():
 
 
 
+if __name__ == "__main__":
+    #ode_model_pressure()
+    #ode_model_concentration()
+    #stock_population()
+    plot_given_data()
+
+
+
+
 #####################################################################################################
 #-functions for odes (pressure and concentration)
 #-function for reading in stock data
@@ -321,8 +306,67 @@ def plot_benchmark():
 #trent was here
 
 
-if __name__ == "__main__":
-    #ode_model_pressure()
-    #ode_model_concentration()
-    #stock_population()
-    plot_given_data()
+# def improved_euler_pressure(f,t0, t1, dt, x0, pars):
+#     """Solve an ODE numerically.
+
+#         Parameters:
+#         -----------
+#         f : callable
+#             Function that returns dxdt given variable and parameter inputs.
+#         t0 : float
+#             Initial time value
+#         t1 : float
+#             Final time value
+#         dt : float
+#             Time step used
+#         x0 : float
+#             Initial value of solution.
+#         pars : array-like
+#             List of parameters passed to ODE function f.
+
+#         Returns:
+#         --------
+#         x : array-like
+#             Dependent variable solution vector.
+
+#         Notes:
+#         ------
+#         Assume that ODE function f takes the following inputs, in order:
+#             1. independent variable
+#             2. dependent variable
+#             3. Parameters that will later be passed to the curve fitting function
+#             4. all other parameters
+#             Refer to the function definitions for order within (3) and (4)
+#             5. Optional - counter number to be passed to array parameters
+#     """
+
+#     # Allocate return arrays
+#     t = np.arange(t0, t1+dt, dt)
+#     params_unknown, params_known = pars
+#     x = np.zeros(len(t))
+#     x[0] = x0
+#     i=0
+#     # Check if q is iterable or const
+#     if isinstance(params_known[0], float) == True:
+#         dqdt = 0
+#         if len(params_known) != 2:
+#             params_known.append(dqdt)
+#         # Loop through time values, finding corresponding x value
+#         for i in range(0, (len(t) - 1)):
+#             # Compute normal euler step
+#             x_temp = x[i] + dt*f(t[i], x[i], params_unknown, params_known)
+#             # Corrector step
+#             x[i+1] = x[i] + (dt/2)*(f(t[i], x[i], params_unknown, params_known) + f(t[i+1], x_temp, params_unknown, params_known))
+#     else:
+#         # Get dqdt and append to known parameters
+#         dqdt = np.gradient(params_known[0])
+#         if len(params_known) != 2:
+#             params_known.append(dqdt)
+#         # Loop through time values, finding corresponding x value
+#         for i in range(0, (len(t) - 1)):
+#             # Compute normal euler step
+#             x_temp = x[i] + dt*f(t[i], x[i], params_unknown, params_known, i=i)
+#             # Corrector step
+#             x[i+1] = x[i] + (dt/2)*(f(t[i], x[i], params_unknown, params_known, i=i) + f(t[i+1], x_temp, params_unknown, params_known, i=i))
+        
+#     return x
